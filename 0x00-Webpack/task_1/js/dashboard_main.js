357628ee1255854d6624e15aad4a1d50cc98b4e0
Rajae <hemmanyrajae@gmail.com>
2024-11-01 02:28:44 +0000

    up

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,16 +1,16 @@
 import $ from 'jquery';
 import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
 let count = 0;
 function updateCounter() {
-	count += 1;
-	$('#count').text(`${count} clicks on the button`);
-}
-
-$(document).ready(function() {
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button>Click here to get started</button>');
-    $('body').append("<p id='count'></p>");
-    const debouncedUpdateCounter = _.debounce(updateCounter, 300);
-    $('#clickButton').on('click', debouncedUpdateCounter);
-});
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+};
+const debouncedUpdateCounter = _.debounce(updateCounter, 300);
+$('#clickButton').on('click', debouncedUpdateCounter);
