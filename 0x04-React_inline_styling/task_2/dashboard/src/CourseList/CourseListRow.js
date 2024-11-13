4c6d3557f7e89f2e9216e1a34abd4df11daf77d5
Rajae <hemmanyrajae@gmail.com>
2024-11-13 04:03:01 +0000

    task 2

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
 function CourseListRow({ isHeader = false, textFirstCell, textSecondCell = null }) {
-  if (isHeader) {
-    if (textSecondCell === null) {
-      return (
-        <tr>
-          <th colSpan="2">{textFirstCell}</th>
-        </tr>
-      );
-    } else {
-      return (
-        <tr>
-          <th>{textFirstCell}</th>
-          <th>{textSecondCell}</th>
-        </tr>
-      );
-    }
-  } else {
-    return (
-      <tr>
-        <td>{textFirstCell}</td>
-        <td>{textSecondCell}</td>
-      </tr>
-    );
-  }
+return (
+    <tr className={css(isHeader ? styles.headerRow : styles.defaultRow)}>
+      {isHeader ? (
+        textSecondCell === null ? (
+          <th colSpan="2" className={css(styles.headerCell)}>{textFirstCell}</th>
+        ) : (
+          <>
+            <th className={css(styles.headerCell)}>{textFirstCell}</th>
+            <th className={css(styles.headerCell)}>{textSecondCell}</th>
+          </>
+        )
+      ) : (
+        <>
+          <td className={css(styles.defaultCell)}>{textFirstCell}</td>
+          <td className={css(styles.defaultCell)}>{textSecondCell}</td>
+        </>
+      )}
+    </tr>
+  );
 }
 
 CourseListRow.propTypes = {
@@ -36,4 +33,24 @@ CourseListRow.propTypes = {
   ])
 };
 
+const styles = StyleSheet.create({
+  headerRow: {
+    backgroundColor: '#f5f5f5',
+  },
+  defaultRow: {
+    backgroundColor: '#f9f9f9',
+  },
+  headerCell: {
+    fontWeight: 'bold',
+    textAlign: 'left',
+    borderBottom: '2px solid #ddd',
+    padding: '8px',
+  },
+  defaultCell: {
+    textAlign: 'left',
+    padding: '8px',
+    borderBottom: '1px solid #ddd',
+  },
+});
+	
 export default CourseListRow;
--- 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
 import NotificationItems from './NotificationItem';
 
 describe('Notification item tests', () => {
@@ -8,7 +7,11 @@ describe('Notification item tests', () => {
     const onclick = jest.fn();
     
     const wrapper = shallow(<NotificationItems markAsRead={onclick} id='1' />);
-    wrapper.find('li').first().simulate('click');
-    expect(onclick).toBeCalled();
+    wrapper.find('li').simulate('click');
+    expect(onclick).toHaveBeenCalledWith(1);
   })
+  it('should apply urgent style when type is urgent', () => {
+    const wrapper = shallow(<NotificationItems type="urgent" />);
+    expect(wrapper.find('li').hasClass('urgent')).toBe(true);
+  });
 })
